cd9a7cc24c884384c6d2998d9e2bbf29f06ad840
Nick-Aaron <1553533356@qq.com>
2017-04-18 22:19:45 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,113 @@
+import pygame, sys, random
+from pygame.locals import *
+
+WINDOWWIDTH = 1000
+WINDOWHEIGHT = 680
+TEXTCOLOR = (255, 255, 255)
+BACKGROUNDCOLOR = (0, 0, 0)
+FPS = 40
+BADDIEMINSIZE = 10
+BADDIEMAXSIZE = 40
+BADDIEMINSPEED = 1
+BADDIEMAXSPEED = 8
+ADDNEWBADDIERATE = 6
+PLAYERMOVERATE = 5
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+    
+def waitForPlayerToPressKey():
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                terminate()
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    terminate()
+                return
+                
+def playerHasHitBaddie(playerRect, baddies):
+     for b in baddies:
+          if playerRect.colliderect(b['rect']):
+              return True
+     return False
+
+def drawText(text, font, surface, x, y):
+    textobj = font.render(text, 1, TEXTCOLOR)
+    textrect = textobj.get_rect()
+    textrect.topleft = (x, y)
+    surface.blit(textobj, textrect)
+    
+# set up the pygame, the window, and the mouse cursor
+pygame.init()
+mainClock = pygame.time.Clock()
+windowSurface = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
+pygame.display.set_caption("dodger v1.9.6")
+pygame.mouse.set_visible(False)
+
+# set up font
+font = pygame.font.SysFont(None, 60)
+font1 = pygame.font.SysFont(None, 40)
+
+# set up sounds
+gameOverSound = pygame.mixer.Sound('resources\\gameOver.wav')
+pygame.mixer.music.load('resources\\background.wav')
+
+# set up images
+playerImage = pygame.image.load('resources\\player.png')
+playerRect = playerImage.get_rect()
+baddieImage = pygame.image.load('resources\\baddie.png')
+
+# draw the "Start" screen
+drawText('Dodger', font, windowSurface, WINDOWWIDTH / 3 + 100, WINDOWHEIGHT / 3)
+drawText('Press any key to start the game', font1, windowSurface, WINDOWWIDTH / 3 - 30, WINDOWHEIGHT / 2 + 50)
+pygame.display.update()
+waitForPlayerToPressKey()
+
+topScore = 0
+while True:
+    # set up the start of the game
+    baddie = []
+    score = 0
+    playerRect.topleft = (WINDOWWIDTH - 2, WINDOWHEIGHT - 50)
+    moveLeft = moveUp = moveRight = moveDown = False
+    reviseCheat = slowCheat = False
+    baddieAddCounter = 0
+    pygame.mixer.music.play(-1, 0.0)
+    
+    while True:  # The game loop runs while the game part is playing
+        score += 1  # Increase score
+        
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                terminate()
+            if event.type == KEYDOWN:
+                if event.key == K_LEFT or event.key == ord('a'):
+                    moveLeft = True
+                    moveRight = False
+                if event.key == K_RIGHT or event.key == ord('d'):
+                    moveRight = True
+                    moveLeft = False
+                if event.key == K_UP or event.key == ord('w'):
+                    moveUp = True
+                    moveDown = False
+                if event.key == K_DOWN or event.key == ord('s'):
+                    moveDown = True
+                    moveUp = False
+
+            if event.type == KEYUP:
+                if event.key == K_LEFT or event.key == ord('a'):
+                    moveLeft = False
+                if event.key == K_RIGHT or event.key == ord('d'):
+                    moveRight = False
+                if event.key == K_UP or event.key == ord('w'):
+                    moveUp = False
+                if event.key == K_DOWN or event.key == ord('s'):
+                    moveDown = False
+                if event.key == K_ESCAPE:
+                    terminate()
+
+            if event.type == MOUSEMOTION:
+                # If the mouse moves, move the player to where the cursor is.
+                playerRect.move_ip(event.pos[0] - playerRect.centerx, event.pos[1] - playerRect.centery)
